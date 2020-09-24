a68717769b985dd2e9e14ea4e0e0a61db5c3beab
2dam <2dam@HZ341354.TartangaLH.eus>
2020-09-24 09:04:49 +0200

    First commit

---
+++ src/reto0dintxt/view/View.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto0dintxt.view;
+
+/**
+ * Declares the metod/s of the view
+ * @author Bryssa
+ */
+public interface View {
+    /**
+     * Show a greeting in UI
+     * @param greeting String containing the greeting
+     */
+    public void showGreeting(String greeting);
+}
---
+++ src/reto0dintxt/view/ViewImplementation.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto0dintxt.view;
+
+/**
+ *  Implementation of the view that shows a text message
+ * @author Bryssa
+ */
+public class ViewImplementation implements View {
+    /**
+     * Show a greeting in UI
+     * @param greeting String containing the greeting
+     */
+    @Override
+    public void showGreeting(String greeting){
+        System.out.println(greeting);
+    }
+}
